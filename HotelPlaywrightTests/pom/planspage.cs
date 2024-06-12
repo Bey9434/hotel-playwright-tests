8b9b2376bf492fefb83b8dbe0c03292d41d6c23f
Bey9434 <aluca9434@gmail.com>
2024-06-12 23:29:37 +0900

    コード整理

--- HotelPlaywrightTests/pom/planspage.cs
+++ HotelPlaywrightTests/pom/planspage.cs
@@ -17,13 +17,16 @@ namespace HotelBookingTests.Pages
 
         public async Task<string[]> GetPlanTitlesAsync()
         {
-            await _page.WaitForSelectorAsync(_planTitlesSelector);
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            //await _page.WaitForSelectorAsync(_planTitlesSelector);
             return await _page.EvaluateAsync<string[]>("Array.from(document.querySelectorAll('.card-title')).map(el => el.textContent)");
         }
          public async Task GoToPlansPageAsync()
         {
             await _page.ClickAsync(_plansLink);
-            await _page.WaitForSelectorAsync(".card-title", new PageWaitForSelectorOptions { Timeout = 10000 });
+            //await _page.WaitForSelectorAsync( _planTitlesSelector, new PageWaitForSelectorOptions { Timeout = 10000 });
+            //await _page.WaitForURLAsync("**/plans.html");
+            //await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
         }
     }
 }
